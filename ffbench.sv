8e21f5d95a6b3c24f5f25c2991ce2b6c66cde327
lcr2131 <robinson.leonardc@gmail.com>
2012-10-07 06:09:21 -0400

    Flip Flop Updates

--- ffbench.sv
+++ ffbench.sv
@@ -1,1 +1,24 @@
 //Author: Leonard Robinson
+class;
+
+
+	function void golden_result;
+	  $display();
+	end function
+
+	function bit check_result();
+		return;
+	end function
+end class
+
+program (iffc.bench x) //iffc is the interface of the flip flop class
+
+	initial begin
+	 repeat
+	  $display
+	  $display
+	 end //end repeat
+	end //end initial
+	
+
+end program
--- flipflop.sv
+++ flipflop.sv
@@ -1,23 +1,13 @@
 // Enabled flip-flop with comparator logic
-module flipflop
-(
-    input 	 clk,
-    input 	 reset, 
-    input 	 data_i,
-    input 	 write_enable_i,
-    input 	 search_enable_i,
-    input 	 search_i,
-    output reg 	 data_o,
-    output logic match_o
-);
+module flipflop(iffc.dut d);
 
-   always_ff @(posedge clk) begin
-      if (reset) data_o <= '0;
-      else if (write_enable_i) data_o <= data_i;
+   always_ff @(posedge d.clk) begin
+      if (d.reset) d.data_o <= '0;
+      else if (write_enable_i) d.data_o <= d.data_i;
    end
 
    always_comb begin
-      match_o = search_enable_i & (search_i == data_o);
+      d.match_o = d.search_enable_i & (d.search_i == d.data_o);
    end
   
 endmodule
