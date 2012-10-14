eb26f9ad354970e42cd55a2f574308fccee3ab68
lcr2131 <robinson.leonardc@gmail.com>
2012-10-13 21:50:58 -0400

    Bench Real Big

--- CAM_Validation/bench.sv
+++ CAM_Validation/bench.sv
@@ -1,1 +1,76 @@
-testbench yo
+//Author:
+//Name:
+//Date Created:Saturday October 13, 2012
+//Date Modified:
+
+class reset;
+
+   function void golden_result;
+
+
+
+   endfunction //
+
+   function void checkbits;
+      
+
+   endfunction //
+
+end class // UNMATCHED !!
+
+class rw;
+
+
+   function void golden_result;
+
+
+
+
+         endfunction //
+
+   function void checkbits;
+
+
+
+         endfunction //
+
+end class // UNMATCHED !!
+
+class search;
+
+
+
+   function void golden_result;
+
+
+
+
+         endfunction //
+
+   function void checkbits;
+
+
+
+         endfunction //
+
+   end class // UNMATCHED !!
+
+   
+program()
+   
+   reset;
+   rw;
+   search;
+
+
+   initial begin
+      repeat(10000) begin //MAX_TRANSACTIONS
+
+      end // end repeat
+   end // end initial
+ 
+   
+endprogram
+     
+   
+   
