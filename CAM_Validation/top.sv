578c9cce5f97aa89e967d6629c60f5ceb89b7cf0
lcr2131 <robinson.leonardc@gmail.com>
2012-10-15 14:21:02 -0400

    Added a top module.

--- CAM_Validation/top.sv
+++ CAM_Validation/top.sv
@@ -1,1 +1,21 @@
-top file, real big
+//Author:
+//Name: top.sv
+//Date Created: October 15, 2012
+//Date Modified: October 15, 2012
+
+`timescale 1ns/1ns
+
+module top();
+   bit clk_i = 0;
+   always #5 clk_i = ~clk_i;
+
+   initial $vcdpluson;
+
+   cam_interface IFC(clk_i); // instantiate the interface file
+   cam_module dut(IFC.dut);
+   tb bench(IFC.bench);
+   
+
+
+
+endmodule // top
