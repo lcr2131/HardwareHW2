b9860e7f2d4809929bbf4d55224f6eac607ec48c
lcr2131 <robinson.leonardc@gmail.com>
2012-10-15 23:50:52 -0400

    Flip Flop Interfacing Complete

--- CAM_Validation/flipflop.sv
+++ CAM_Validation/flipflop.sv
@@ -1,27 +1,17 @@
 // Author: Corey Stappenbeck
 // Module: flipflop.sv
 // Date Created: September 25, 2012
-// Date Modified: October 1, 2012
+// Date Modified: October 15, 2012
 
-module flipflop
-  (
-   input 	clk,
-   input 	reset, 
-   input 	data_i,
-   input 	write_enable_i,
-   input 	search_enable_i,
-   input 	search_i,
-   output reg 	data_o,
-   output logic match_o
-   );
+module flipflop (cam_interface.dut_flipflop d);
 
-   always_ff @(posedge clk) begin
-      if (reset) data_o <= '0;
-      else if (write_enable_i) data_o <= data_i;
+   always_ff @(posedge d.clk_i) begin
+      if (d.ff_reset) d.ff_data_o <= '0;
+      else if (d.ff_write_enable_i) d.ff_data_o <= d.ff_data_i;
    end
 
    always_comb begin
-      match_o = search_enable_i & (search_i == data_o);
+      d.ff_match_o = d.ff_search_enable_i & (d.ff_search_i == d.ff_data_o);
    end
    
 endmodule
