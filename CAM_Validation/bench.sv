27dce9ee852204e8383a39a9fa257588377d5315
TheDonald <thedonald@ubuntu.(none)>
2012-10-17 14:26:27 -0400

    Added body to testbench program

--- CAM_Validation/bench.sv
+++ CAM_Validation/bench.sv
@@ -39,24 +39,25 @@ class cam_test;
 	int map[31:0];	
 
 	function void reset_golden_result;
-		if(reset == 1)
-			for(i=0;i<32;i=i+1)
+		if(reset == 1) begin
+			for(i=0;i<32;i=i+1) begin
 				map[i]=0;
 			end
 		end
 	endfunction;
 
 	function void rw_golden_result;
-		if(read_enable==1)
-			if((read_address < 32) & (read_address>-1))
+		if(read_enable==1) begin
+			if((read_address < 32) & (read_address>-1)) begin
 				read_value = map[read_address];
 				read_valid = 1;
-			else
+			else begin
 				read_valid==0;	
+			end
 		end
 
-		if(write_enable==1)
-			if((write_address < 32) & (write_address>-1))
+		if(write_enable==1) begin
+			if((write_address < 32) & (write_address>-1)) begin
 				map[write_address] = write_data;							
 			end
 		end
@@ -64,12 +65,12 @@ class cam_test;
 
 	function void search_golden_result;
 		search_valid = 0;		
-		if(search_enable==1)
+		if(search_enable==1) begin
 			for(i=0;i<32;i=i+1)
-				if(map[i]==search_data)
+				if(map[i]==search_data) 
 					search_valid = 1;
 					search_index = i;					
-				break;
+					break;
 			end
 		end
 	endfunction
@@ -138,34 +139,57 @@ endclass
 
 program testbench (cam_interface.bench_cam cam_tb);
    
-   cam_transaction t;
-   reset;
-   rw;
-   search;
+	cam_transaction packet;
+  	cam_test test;
+	cam_checker check;
+	cam_env env;
+	int cycle;
 
+	task do_cycle
+		env.cycle++;
+		cycle = env.cycle;
+		cycle = env.cycle;
+       		packet = new();
+        	packet.randomize();
+		cam_tb.cb.rst_i <= packet.reset;
+		cam_tb.cb.read_enable_i <= packet.read_enable; 
+		cam_tb.cb.read_index_i <= packet.read_address;
+		cam_tb.cb.write_enable_i <= packet.write_enable;
+		cam_tb.cb.write_index_i <= packet.write_address;
+		cam_tb.cb.write_data_i <= packet.write_data;
+   		cam_tb.cb.search_enable_i <= packet.search_enable;
+		cam_tb.cb.search_data_i <= packet.search_data;	
+		@(cam_tb.cb);
+		test.reset_golden_result();
+		test.rw_golden_result();
+		test.search_golden_result();
+	endtask
 
-	initial begin
-      		repeat(10000) begin //MAX_TRANSACTIONS
-			t = new();
-     			t.randomize();
+  	initial begin
+        	test = new();
+        	check = new();
+        	packet = new();
+        	env = new();
+        	env.configure("config.txt");
 
-			// drive inputs for next cycle
-			$display("%t : %s \n", $realtime, "Driving New Values");  
+        	// warm up
+        	repeat (env.warmup_time) begin
+            		do_cycle();
+        	end
 
-		 	cam_tb.cb.rst_i <= t.reset;
-		 	cam_tb.cb.read_enable_i <= t.read_enable; 
-			cam_tb.cb.read_index_i <= t.read_address;
-			cam_tb.cb.write_enable_i <= t.write_enable;
-			cam_tb.cb.write_index_i <= t.write_address;
-		 	cam_tb.cb.write_data_i <= t.write_data;
-   		 	cam_tb.cb.search_enable_i <= t.search_enable;
-			cam_tb.cb.search_data_i <= t.;								  
-      			
-			@(cam_tb.cb);
-      				
+        	// testing
+        	repeat (env.max_transactions) begin
+            		do_cycle();
 
-		end // end repeat
-	end // end initial
+            		if(checker.bit_check_result(ds.cb.read_valid_o,test.read_valid,env.verbose)==1) begin
+				checker.int_check_result(ds.cb.read_value_o, test.read_value, env.verbose);
+			end
+
+			if(checker.bit_check_result(ds.cb.search_valid_o,test.search_valid,env.verbose)==1) begin
+				checker.int_check_result(ds.cb.search_index_o, test.search_index, env.verbose);
+			end			
+        	end
+	end
    
    
 endprogram 
